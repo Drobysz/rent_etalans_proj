9da9c192ca7e4426c74d590cfdb43688728cb443
Drobysz <alexanderdrobyschevski@gmail.com>
2026-06-11 19:03:42 +0200

    chore(app): add UI dependencies and font setup

---
+++ app/fonts/fonts.ts
@@ -0,0 +1,89 @@
+import { 
+    Plus_Jakarta_Sans, Inter, Climate_Crisis,
+    B612, Bagel_Fat_One, Trade_Winds,
+    Bilbo_Swash_Caps, Geist
+} from "next/font/google";
+
+export const geist = Geist({
+    variable: "--font-geist",
+});
+
+export const bilbo_swash_caps = Bilbo_Swash_Caps({
+    weight: "400",
+    variable: "--font-bilbo-swash-caps",
+});
+
+export const trade_winds = Trade_Winds({
+    weight: "400",
+    variable: "--font-trade-winds",
+});
+
+export const crisis = Climate_Crisis({
+    weight: "400",
+    variable: "--font-crisis",
+});
+
+export const bagel = Bagel_Fat_One({
+    weight: "400",
+    variable: "--font-bagel",
+});
+
+export const b612_regular = B612({
+    weight: "400",
+    variable: "--font-b612",
+});
+
+export const b612_bold = B612({
+    weight: "700",
+    variable: "--font-b612-bold",
+});
+
+export const inter_regular = Inter({
+    weight: "400",
+    variable: "--font-inter-regular",
+});
+
+export const inter_medium = Inter({
+    weight: "500",
+    variable: "--font-inter-medium",
+});
+
+export const inter_semibold = Inter({
+    weight: "600",
+    variable: "--font-inter-semibold",
+});
+
+export const inter_bold = Inter({
+    weight: "700",
+    variable: "--font-inter-bold",
+});
+
+export const inter_extrabold = Inter({
+    weight: "800",
+    variable: "--font-inter-extrabold",
+});
+
+export const jakarta_regular = Plus_Jakarta_Sans({
+    weight: "400",
+    variable: "--font-jakarta-regular",
+});
+
+export const jakarta_medium = Plus_Jakarta_Sans({
+    weight: "500",
+    variable: "--font-jakarta-medium",
+});
+
+export const jakarta_semibold = Plus_Jakarta_Sans({
+    weight: "600",
+    variable: "--font-jakarta-semibold",
+});
+
+export const jakarta_bold = Plus_Jakarta_Sans({
+    weight: "700",
+    variable: "--font-jakarta-bold",
+});
+
+export const jakarta_extrabold = Plus_Jakarta_Sans({
+    weight: "800",
+    variable: "--font-jakarta-extrabold",
+});
